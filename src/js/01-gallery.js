3ccb296ce512e72d17922eab4c598c3baec538a8
aliesia-zahorui <aliesia@ukr.net>
2022-09-20 11:03:37 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+const galleryMarkup = createGallerySmallPictureCard(galleryItems);
+
+galleryList.innerHTML = galleryMarkup;
+
+function createGallerySmallPictureCard(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+// 2) Підключення скрипту і стилів бібліотеки, використовуючи CDN сервіс cdnjs.
+// Необхідно додати посилання на два файли: simple-lightbox.min.js і simple-lightbox.min.css.
+//done
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
